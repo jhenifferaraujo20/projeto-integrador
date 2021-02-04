efd0133129d705744b10e6f2a947727d3ba28f1c
jhenifferaraujo20 <73621749+jhenifferaraujo20@users.noreply.github.com>
2021-02-03 23:26:26 -0300

    Add files via upload

---
+++ js/funcoes.js
@@ -0,0 +1,8 @@
+/* ==================== LOGIN/ CADASTRO ==================== */
+function abrirLogin(){
+    document.querySelector(".popup-login-cadastro").style.display = "flex";
+}
+
+function fecharLogin(){
+    document.querySelector(".popup-login-cadastro").style.display = "none";
+}
