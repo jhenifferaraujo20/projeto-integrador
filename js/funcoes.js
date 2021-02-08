8d87b4a315943583da4e5b1937a08d5273860632
jhenifferaraujo20 <73621749+jhenifferaraujo20@users.noreply.github.com>
2021-02-08 19:29:55 -0300

    Add files via upload

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,8 +1,39 @@
 /* ==================== LOGIN/ CADASTRO ==================== */
-function abrirLogin(){
-    document.querySelector(".popup-login-cadastro").style.display = "flex";
+/*function abrirLogin(){
+    document.querySelector(".modal-login-cadastro").style.display = "flex";
 }
 
 function fecharLogin(){
-    document.querySelector(".popup-login-cadastro").style.display = "none";
-}
+    document.querySelector(".modal-login-cadastro").style.display = "none";
+}*/
+
+$(document).on('click','.user,.voltarlogin',function(){
+    $('.modal-login-cadastro').addClass('login-active').removeClass('cadastro-active')
+});
+
+$(document).on('click', '.cadastro',function(){
+    $('.modal-login-cadastro').addClass('cadastro-active').removeClass('login-active')
+});
+
+$(document).on('click', '.fechar-login',function(){
+    $('.modal-login-cadastro').removeClass('login-active').removeClass('cadastro-active')
+});
+
+let modal = document.querySelector(".modal-login-cadastro");
+window.onclick = function(event) {
+    if(event.target == modal) {
+        modal.classList.remove("login-active");
+        modal.classList.remove("cadastro-active");
+    }
+};
+
+/* =================== PRODUTOS SLIDER ===================== */
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
